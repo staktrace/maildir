6b112d74626b05c2761aa67c0f952d3c95603fd6
Kartikaya Gupta <kats@seldon.staktrace.com>
2017-01-14 22:53:11 -0500

    Add documentation

---
+++ target/doc/implementors/core/fmt/trait.Display.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["maildir"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/fmt/trait.Display.html' title='core::fmt::Display'>Display</a> for <a class='enum' href='maildir/enum.MailEntryError.html' title='maildir::MailEntryError'>MailEntryError</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ target/doc/maildir/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"enum":[["MailEntryError",""]],"struct":[["MailEntries",""],["MailEntry",""],["Maildir",""]]});
