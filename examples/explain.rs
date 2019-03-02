6f122bdaf48874e57b9e34984881058e23b07d3f
Matthias Beyer <mail@beyermatthias.de>
2019-03-02 19:08:34 +0100

    Add example to check mail flags
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

---
+++ examples/explain.rs
@@ -0,0 +1,41 @@
+extern crate maildir;
+
+use std::path::PathBuf;
+use maildir::Maildir;
+use maildir::MailEntry;
+
+fn unwrap_mail(mail: ::std::io::Result<MailEntry>) -> MailEntry {
+    mail.unwrap_or_else(|e| {
+        eprintln!("Error: {:?}", e);
+        ::std::process::exit(1);
+    })
+}
+
+fn list_mail(mail: MailEntry) {
+    println!("Path:         {}", mail.path().display());
+    println!("ID:           {}", mail.id());
+    println!("Flags:        {}", mail.flags());
+    println!("is_draft:     {}", mail.is_draft());
+    println!("is_flagged:   {}", mail.is_flagged());
+    println!("is_passed:    {}", mail.is_passed());
+    println!("is_replied:   {}", mail.is_replied());
+    println!("is_seen:      {}", mail.is_seen());
+    println!("is_trashed:   {}", mail.is_trashed());
+}
+
+fn main() {
+    // not sure whether this is actually fast or something, but we don't care here, do we?
+    ::std::env::args()
+        .skip(1)
+        .map(PathBuf::from)
+        .map(Maildir::from)
+        .for_each(|mdir| {
+            mdir.list_new()
+                .map(unwrap_mail)
+                .for_each(list_mail);
+
+            mdir.list_cur()
+                .map(unwrap_mail)
+                .for_each(list_mail);
+        });
+}
